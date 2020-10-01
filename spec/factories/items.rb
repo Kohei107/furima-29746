b813566701f9f39edffe883f67f1aa78561e99bd
Kohei107 <k.s107bb@gmail.com>
2020-10-01 11:09:05 +0900

    単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
     
+    name                 { '名前' }
+    detail               { '商品説明' }
+    category_id          {2}
+    condition_id         {5}
+    shipping_fee_id      {1}
+    shipping_place_id    {40}
+    prep_time_id         {2}
+    price                {1000000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    
+    
   end
 end
