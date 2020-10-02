c9c95381c28f781a4241ee1b7912bfaa89b5960c
Kohei107 <k.s107bb@gmail.com>
2020-10-02 12:17:50 +0900

    商品情報編集機能　訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all
@@ -20,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -42,6 +39,10 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to user_session_path unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
