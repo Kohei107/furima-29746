2b9cfceea3e2c8e7e49e86614427bc8f3f6a6eca
Kohei107 <k.s107bb@gmail.com>
2020-10-03 21:44:14 +0900

    Pay.jp決済処理実装(Heroku上に環境変数を設定済)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,35 @@ class OrdersController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :municipality, :street_number, :building, :phone_number, :item_id).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: order_params[:token],
+    currency: 'jpy'
+   )
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ApplicationRecord
   has_one :adress
   belongs_to :item
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :shipping_place
+
+  attr_accessor :token, :postal_code, :prefecture_id, :municipality, :street_number, :building, :phone_number
+
+  validates :token, presence: true
 end
